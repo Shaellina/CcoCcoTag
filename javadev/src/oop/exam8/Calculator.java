1bbbc7500b2051f3d3e67434ab9646e075079169
JB <whdqhd5402@gmail.com>
2017-06-22 09:08:00 +0900

    ~2017/06/21

---
+++ javadev/src/oop/exam8/Calculator.java
@@ -0,0 +1,8 @@
+package oop.exam8;
+
+public class Calculator {
+	public static double pi = 3.141592; // static으로 만들었을 경우,
+										// class area 또는 static area라고 불리는 메모리 영역에 들어가게 된다.
+										// 그래서 static으로 선언 했을 경우, 
+										// 인스턴스를 만들지 않고도 접근이 가능하다.
+}
---
+++ javadev/src/oop/exam8/CalculatorTest.java
@@ -0,0 +1,11 @@
+package oop.exam8;
+
+public class CalculatorTest {
+
+	// static {} -- > static block은 제일 처음 실행 할 것들에 대해 설정해두면 된다.
+	// static block 이라고 한다.
+
+	public static void main(String[] args) {
+		System.out.println(Calculator.pi);
+	}
+}
